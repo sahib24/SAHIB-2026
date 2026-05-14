6855a1b7f8a5c913a18a298a8c2c710c7d5b3b35
Sahib Ahmed <sahibahmed3@gmail.com>
2026-05-14 16:09:18 +0600

    crud may

--- src/app/mayCRUD/page.js
+++ src/app/mayCRUD/page.js
@@ -1,9 +1,9 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
-export default function CRUDMonday() {
+export default function CRUD() {
   const [posts, setPosts] = useState([]);
   const [title, setTitle] = useState("");
   const [editId, setEditId] = useState(null);
@@ -15,8 +15,8 @@ export default function CRUDMonday() {
         "https://jsonplaceholder.typicode.com/posts?_limit=5",
       );
       setPosts(res.data);
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
     }
   };
 
@@ -28,37 +28,32 @@ export default function CRUDMonday() {
     e.preventDefault();
 
     if (!title.trim()) return;
-
     try {
       const res = await axios.post(
         "https://jsonplaceholder.typicode.com/posts",
         {
           title,
-          body: "new post body",
+          body: "create body",
           userId: 1,
         },
       );
-
       setPosts([res.data, ...posts]);
       setTitle("");
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
     }
   };
 
-  // UPDATE
   const handleUpdate = async () => {
     if (!editTitle.trim()) return;
-
     try {
       const res = await axios.put(
         `https://jsonplaceholder.typicode.com/posts/${editId}`,
         {
           title: editTitle,
-          body: "Updated body",
+          body: "update body",
         },
       );
-
       setPosts(
         posts.map((post) =>
           post.id === editId ? { ...post, title: res.data.title } : post,
@@ -67,26 +62,19 @@ export default function CRUDMonday() {
 
       setEditId(null);
       setEditTitle("");
-    } catch (error) {
-      console.log("UPDATE error:", error);
+    } catch (err) {
+      console.log(err);
     }
   };
 
   const handleDelete = async (id) => {
     try {
-      const res = await axios.delete(
-        `https://jsonplaceholder.typicode.com/posts/${id}`,
-      );
+      await axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`);
 
       setPosts(posts.filter((post) => post.id !== id));
-    } catch (error) {
-      console.log(error);
+    } catch (err) {
+      console.log(err);
     }
-  };
-
-  const handleCancel = () => {
-    setEditId(null);
-    setEditTitle("");
   };
 
   const openEdit = (post) => {
@@ -94,27 +82,31 @@ export default function CRUDMonday() {
     setEditTitle(post.title);
   };
 
+  const closeEdit = () => {
+    setEditId(null);
+    setEditTitle("");
+  };
+
   return (
-    <div className="p-5 ">
-      <form onSubmit={handleCreate} className="flex gap-1 mb-4">
+    <div className="p-5">
+      <form onSubmit={handleCreate} className="">
         <input
           type="text"
-          placeholder="Text Data"
+          placeholder="Text data"
           value={title}
           onChange={(e) => setTitle(e.target.value)}
           className="border p-1"
         />
 
-        <button className="p-1 border">Create</button>
+        <button className="border p-1">create</button>
       </form>
 
       <div>
         {posts.map((post) => (
-          <div key={post.id} className="mb-4 border p-3 rounded">
-            {editId === post.id ? (
+          <div key={post.id} className="">
+            {post.id === editId ? (
               <input
                 type="text"
-                placeholder="Text Data"
                 value={editTitle}
                 onChange={(e) => setEditTitle(e.target.value)}
                 className="border p-1"
@@ -123,41 +115,41 @@ export default function CRUDMonday() {
               <p>{post.title}</p>
             )}
 
-            <div className="flex gap-2 mt-3">
-              {editId === post.id ? (
-                <>
+            <div className="flex border">
+              {post.id === editId ? (
+                <div>
                   <button
                     type="button"
-                    onClick={handleUpdate}
                     className=" text-black px-3 py-1 rounded"
+                    onClick={handleUpdate}
                   >
-                    Save
+                    save
                   </button>
 
                   <button
                     type="button"
-                    onClick={handleCancel}
                     className=" text-black px-3 py-1 rounded"
+                    onClick={closeEdit}
                   >
-                    Cancel
+                    close
                   </button>
-                </>
+                </div>
               ) : (
                 <button
                   type="button"
+                  className=" text-black px-3 py-1 rounded"
                   onClick={() => openEdit(post)}
-                  className="text-black px-3 py-1 rounded"
                 >
-                  Edit
+                  edit
                 </button>
               )}
 
               <button
                 type="button"
-                onClick={() => handleDelete(post.id)}
                 className=" text-black px-3 py-1 rounded"
+                onClick={() => handleDelete(post.id)}
               >
-                Delete
+                delete
               </button>
             </div>
           </div>
--- src/app/mayEnd/page.js
+++ src/app/mayEnd/page.js
@@ -5,7 +5,7 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 
 export default function MayEnd() {
-  const [post, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [title, setTitle] = useState("");
   const [editID, setEditId] = useState(null);
   const [editTitle, setEditTitle] = useState("");
@@ -15,7 +15,7 @@ export default function MayEnd() {
       const res = await axios.get(
         "https://jsonplaceholder.typicode.com/posts?_limit=5",
       );
-      setPost(res.data);
+      setPosts(res.data);
     } catch (err) {
       console.log(err);
     }
@@ -24,5 +24,139 @@ export default function MayEnd() {
     fetchData();
   }, []);
 
-  return <div>jfwhfbvsbhb</div>;
+  const handleCreate = async (e) => {
+    e.preventDefault();
+
+    if (!title.trim()) return;
+
+    try {
+      const res = await axios.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        {
+          title,
+          body: "post body",
+          userId: 1,
+        },
+      );
+      setPosts([res.data, ...posts]);
+      setTitle("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUpdate = async () => {
+    if (!editTitle.trim()) return;
+
+    try {
+      const res = await axios.put(
+        `https://jsonplaceholder.typicode.com/posts/${editID}`,
+        {
+          title: editTitle,
+          body: "update body",
+        },
+      );
+      setPosts(
+        posts.map((post) =>
+          post.id === editID ? { ...post, title: res.data.title } : post,
+        ),
+      );
+      setEditId(null);
+      setEditTitle("");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      const res = await axios.delete(
+        `https://jsonplaceholder.typicode.com/posts/${id}`,
+      );
+
+      setPosts(posts.filter((post) => post.id !== id));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const openEdit = (post) => {
+    setEditId(post.id);
+    setEditTitle(post.title);
+  };
+
+  const openCancle = () => {
+    setEditId(null);
+    setEditTitle("");
+  };
+
+  return (
+    <div className="p-5 ">
+      <form onSubmit={handleCreate} className="flex gap-1 mb-4">
+        <input
+          type="text"
+          placeholder="Text data"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="border p-1"
+        />
+        <button className="p-1 border">Create</button>
+      </form>
+
+      <div>
+        {posts.map((post) => (
+          <div key={post.id} className="mb-4 border p-3 rounded">
+            {post.id === editID ? (
+              <input
+                type="text"
+                value={editTitle}
+                onChange={(e) => setEditTitle(e.target.value)}
+                className="border p-1"
+              />
+            ) : (
+              <p> {post.title}</p>
+            )}
+
+            <div className="flex gap-2 mt-3">
+              {post.id === editID ? (
+                <div>
+                  <button
+                    type="button"
+                    onClick={handleUpdate}
+                    className=" text-black px-3 py-1 rounded"
+                  >
+                    Save
+                  </button>
+
+                  <button
+                    type="button"
+                    onClick={openCancle}
+                    className=" text-black px-3 py-1 rounded"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              ) : (
+                <button
+                  type="button"
+                  onClick={() => openEdit(post)}
+                  className="text-black px-3 py-1 rounded"
+                >
+                  Edit
+                </button>
+              )}
+
+              <button
+                type="button"
+                onClick={() => handleDelete(post.id)}
+                className=" text-black px-3 py-1 rounded"
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
